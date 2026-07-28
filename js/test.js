278c83ef867ece01231f09a792c8ef1ba5383153
Tanusri-Reddy <tanusrireddy19@gmail.com>
2026-07-28 10:45:10 +0000

    js

---
+++ js/test.js
@@ -0,0 +1,10 @@
+console.log ("hello world")
+let student_name = "Tanusri Reddy"
+console.log("Student name is: " + student_name)
+let student_age =21
+let course_name="python full stack"
+console.log("student age is :" + student_age ,"course name is :" +coursename)
+let fee =10000
+let discount = 10
+let discounted_fee = fee - (fee * discount /100)
+console.log("discounted fee is :" + discounted_fee)
